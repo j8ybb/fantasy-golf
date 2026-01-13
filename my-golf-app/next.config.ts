49316283d3d5f3062e9c0ba37f70b17c021411ae
j8ybb <49569346+j8ybb@users.noreply.github.com>
2026-01-13 22:09:42 +0000

    Logos

--- my-golf-app/next.config.ts
+++ my-golf-app/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"], // Explicitly allow AVIF
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
